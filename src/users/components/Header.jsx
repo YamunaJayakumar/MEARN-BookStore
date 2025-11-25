aec73f60102545fa3356466ef9916480783f71ec
YamunaJayakumar <yamunajayakumar789@gmail.com>
2025-11-25 12:58:08 +0530

    'home'

--- src/users/components/Header.jsx
+++ src/users/components/Header.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBars, FaFacebook, FaInstagram, FaTwitter, FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+Link
 
 function Header() {
+  const [listStatus, setListStatus] = useState(false)
+  const menuBtnClcik=()=>{
+    setListStatus(!listStatus)
+  }
   return (
-    <div>Header</div>
+    <>
+      {/*Header upperpart- title &login */}
+      <div className="grid grid-cols-3 p-3">
+        {/* logo with title */}
+        <div className='flex items-center'>
+          <img width={'60px'} height={'60px'} src="https://icon-library.com/images/books-icon/books-icon-1.jpg" alt="logo" />
+          <h1 className="text-2xl font-bold ms-1 md:hidden">BOOKSTORE</h1>
+        </div>
+        {/* title */}
+        <div className='md:flex items-center justify-center hidden'>
+          <h1 className='text-3xl font-bold'>BOOKSTORE</h1>
+        </div>
+        {/* login */}
+        <div className='md:flex items-center justify-end hidden'>
+          <FaInstagram />
+          <FaFacebook className='mx-2' />
+          <FaTwitter />
+          <Link to={'/login'} className='ms-4 border rounded px-2 py-1 flex items-center hover:bg-black hover:text-white'><FaUser />login</Link>
+
+        </div>
+
+      </div>
+      {/* header lower part-links menu +login */}
+      <nav className="w-full p-2 bg-black text-light md:flex items-center justify-center items-center">
+        {/* div-menu-bar=login */}
+        <div className="flex items-center justify-between text-2xl text-white md:hidden ">
+          {/* menubar */}
+          <button onClick={menuBtnClcik}><FaBars /></button>
+          {/* loginlink */}
+          <Link to={'/login'} className='ms-4 border rounded px-2 py-1 hover:bg-white hover:text-black flex items-center'>
+            <FaUser className='me-1' />login</Link>
+
+        </div>
+        {/* ui-links */}
+        <ul className={listStatus?"flex flex-col text-white":" text-white md:flex items-center justify-center items-center hidden"}>
+          <li className='md:mx-4 mt-3 md:mt-0'><Link to={'/'} >HOME</Link></li>
+          <li  className='md:mx-4 my-4 md:my-0'><Link to={'/books'}>BOOKS</Link></li>
+          <li className=' md:mx-4 mb-3 md:mb-0'><Link to={'/contact'} >CONTACT</Link></li>
+        </ul>
+
+      </nav>
+    </>
   )
 }
 
--- src/users/pages/Home.jsx
+++ src/users/pages/Home.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import Header from '../components/Header'
+import Footer from '../../components/Footer'
+import { FaSearch } from 'react-icons/fa'
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Header />
+      <div>
+        {/* landingpart-search */}
+        <div style={{ height: '500px' }} className="w-full flex justify-center items-center flex-col bg-[url(/bgnew.jpg)] bg-cover bg-center">
+          <div style={{ height: '500px', background: 'rgba(0,0,0,0.4)' }} className="w-full flex justify-center items-center flex-col text-white ">
+            <h1 className='text-5xl font-bold mb-2 '>Wonderful Gifts</h1>
+            <p>Gift your family and friend a book</p>
+            {/* search */}
+            <div className=" mt-5 flex ">
+              <input type="text" className='bg-white rounded-2xl text-black w-100 placeholder-gray-500 p-2' placeholder="search your book here" />
+              <butto className='text-gray-500 my-auto' style={{ marginLeft: "-40px" }}>
+                <FaSearch />
+              </butto>
+            </div>
+          </div>
+        </div>
+        {/* new arrival */}
+        <section className="md: px-40 p-5 my-5 flex items-center justify-center flex-col">
+          <h1 className="text-3xl font-bold">NEW ARRIVALS</h1>
+          <h2 className="text-2xl ">Explore Our Latest Collections</h2>
+        {/* bookrowand col */}
+        </section>
+        {/* author */}
+
+        {/* testimonials */}
+      </div>
+      <Footer /></div>
   )
 }
 
