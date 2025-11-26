5a87d0d480b8a4fdda87c25669d1f77012dff1fb
YamunaJayakumar <yamunajayakumar789@gmail.com>
2025-11-26 12:58:18 +0530

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import { FaArrowRight, FaFacebook, FaHeart, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <div  className="w-full flex flex-col ps-5 gap-10  items-start md:grid grid-cols-3 text-white bg-black items-center gap-4 py-10">
+        <div className="flex flex-col ms-2 gap-2  ">
+          <h4 className="text-2xl ">
+            About Us
+          </h4>
+          <p className='text-justify pe-10'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique itaque vitae ipsam amet vero placeat voluptatem nostrum sed! Sapiente ex quia enim ipsa voluptatem ratione cum ad nisi unde iure?
+          </p>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h4 className="text-2xl ">
+            News Letter
+          </h4>
+          <p className='text-justify pe-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit
+          </p>
+          <div className="flex justify-start items-center w-full">
+            <input type="text" placeholder="Email ID" className='placeholder-gray-500 border bg-white w-75 py-2' />
+            <button className='text-gray-500 my-auto  bg-yellow-500 text-white ' style={{padding:'.83rem'}} >
+              <FaArrowRight />
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col   gap-2">
+          <h4 className="text-2xl ">Follw Us</h4>
+          <p>Lets us be Social</p>
+          <div className="flex justify-start items-center ">
+            <FaInstagram />
+            <FaTwitter className='mx-2' />
+            <FaFacebook className='me-2' />
+            <FaLinkedin />
+          </div>
+        </div>
+        </div>
+        <div className="text-justifymd:w-full flex  justify-center items-center bg-black text-white">
+          <p>copyright &copy; 2025 All rights reserved | This website is made with luminar technolab</p>
+        </div>
+      
+      
+    </>
   )
 }
 
--- src/users/pages/Home.jsx
+++ src/users/pages/Home.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../../components/Footer'
 import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
 function Home() {
   return (
     <div>
       <Header />
       <div>
         {/* landingpart-search */}
-        <div style={{ height: '500px' }} className="w-full flex justify-center items-center flex-col bg-[url(/bgnew.jpg)] bg-cover bg-center">
-          <div style={{ height: '500px', background: 'rgba(0,0,0,0.4)' }} className="w-full flex justify-center items-center flex-col text-white ">
+        <div style={{ height: '600px' }} className="w-full flex justify-center items-center flex-col bg-[url(/bg.jpeg)] bg-cover bg-center">
+          <div style={{ height: '600px', background: 'rgba(0,0,0,0.4)' }} className="w-full flex justify-center items-center flex-col text-white ">
             <h1 className='text-5xl font-bold mb-2 '>Wonderful Gifts</h1>
             <p>Gift your family and friend a book</p>
             {/* search */}
             <div className=" mt-5 flex ">
               <input type="text" className='bg-white rounded-2xl text-black w-100 placeholder-gray-500 p-2' placeholder="search your book here" />
-              <butto className='text-gray-500 my-auto' style={{ marginLeft: "-40px" }}>
+              <button className='text-gray-500 my-auto' style={{ marginLeft: "-40px" }}>
                 <FaSearch />
-              </butto>
+              </button>
             </div>
           </div>
         </div>
@@ -25,11 +27,81 @@ function Home() {
         <section className="md: px-40 p-5 my-5 flex items-center justify-center flex-col">
           <h1 className="text-3xl font-bold">NEW ARRIVALS</h1>
           <h2 className="text-2xl ">Explore Our Latest Collections</h2>
-        {/* bookrowand col */}
+          {/* book rowand col */}
+          <div className="md:grid grid-cols-4 w-full mt-10">
+            <div className="shadow rounded p-3 mx-4">
+              <img width={'300px'} height={'300px'} src="https://m.media-amazon.com/images/I/91ZUvoslh1L._UF1000,1000_QL80_.jpg" alt="book" />
+              <div className="flex  flex-col mt-4 justify-center items-center">
+                <h3 className='text-blue-300 font-bold text-lg'>Autor</h3>
+                <h4 className="text-lg">title</h4>
+                <h4>$ price</h4>
+              </div>
+              
+            </div>
+            {/* duplicate row and cols */}
+            <div className="shadow rounded p-3 mx-4">
+              <img width={'300px'} height={'300px'} src="https://m.media-amazon.com/images/I/91ZUvoslh1L._UF1000,1000_QL80_.jpg" alt="book" />
+              <div className="flex  flex-col mt-4 justify-center items-center">
+                <h3 className='text-blue-300 font-bold text-lg'>Autor</h3>
+                <h4 className="text-lg">title</h4>
+                <h4>$ price</h4>
+              </div>
+              
+            </div>
+            <div className="shadow rounded p-3 mx-4">
+              <img width={'300px'} height={'300px'} src="https://m.media-amazon.com/images/I/91ZUvoslh1L._UF1000,1000_QL80_.jpg" alt="book" />
+              <div className="flex  flex-col mt-4 justify-center items-center">
+                <h3 className='text-blue-300 font-bold text-lg'>Autor</h3>
+                <h4 className="text-lg">title</h4>
+                <h4>$ price</h4>
+              </div>
+            </div>
+            <div className="shadow rounded p-3 mx-4">
+              <img width={'300px'} height={'300px'} src="https://m.media-amazon.com/images/I/91ZUvoslh1L._UF1000,1000_QL80_.jpg" alt="book" />
+              <div className="flex  flex-col mt-4 justify-center items-center">
+                <h3 className='text-blue-300 font-bold text-lg'>Autor</h3>
+                <h4 className="text-lg">title</h4>
+                <h4>$ price</h4>
+              </div>
+            </div>
+          </div>
+          {/* all books link */}
+          <div className="text-center mt-10">
+            <Link to={'/books'} className='px-3 py-2 bg-blue-800'>Explore more...</Link>
+          </div>
         </section>
         {/* author */}
+        <section className=" md:p-5 px-40 my-5 md:grid grid-cols-2 ">
+            {/* autorcontetnt */}
+            <div className='text-center '>
+              <h1 className="text-3xl font-bold">FEATURED AUTOR</h1>
+              <h2 className="text-2xl ">Captivate with every words</h2>
+              <p className='text-justify mt-9'>Driven by a love for storytelling and curiosity about the world, the author believes that words have the power to connect people, heal hearts, and spark imagination. Every story written is a reflection of lived experiences, silent emotions, and the small moments of life that often go unnoticed.
+                 from heartfelt fiction to thoughtful reflections and inspiring themes. The goal of every work is simple: to make the reader feel something and leave a lasting impression long after the last page is turned.
+               </p>
+               <p className='text-justify mt-4' > When not writing, the author can be found reading, observing people, or wandering through nature for inspiration. The journey of writing is ongoing, and each book or blog is a step toward learning, growing, and touching more lives.
 
+                Thank you for being a part of this journey. Your time, support, and love for reading make every word worthwhile.</p>
+                <p className='text-justify mt-4' > When not writing, the author can be found reading, observing people, or wandering through Your time, support, and love for reading make every word worthwhile</p>
+            </div>
+            {/* autor img */}
+            <div className="p-5 flex justify-center items-center">
+              <img className='w-100' src="https://i.pinimg.com/originals/d7/37/94/d737946d153beb56555ed95ab0af1ee1.jpg" alt="" />
+            </div>
+        
+        </section>
         {/* testimonials */}
+        <section className="md:px-40 p-5 my-5 flex items-center justify-center flex-col mt-10">
+          <h1 className="text-3xl font-bold">TESTIMONIALS</h1>
+          <h2 className="text-2xl ">See what thers are saying</h2>
+          <div className="my-5 flex flex-col items-center justify-center">
+            {/* user img */}
+            <img width={'200px'} height={'200px'}  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIf4R5qPKHPNMyAqV-FjS_OTBB8pfUV29Phg&s" alt="" />
+            {/* feedback */}
+            <p className=' mt-5'>Jhon Doe</p>
+            <p className='text-justify mt-4'>"<span className='font-bold me-2'>I was surprised by the quality</span>! The book looked almost new, and the price was unbelievably low. Buying second-hand here feels smart, affordable, and satisfying. I’ll definitely order again.This store has completely changed my view on used books. Every book I received was clean, well-maintained, and carefully packed. It’s the best place for budget-friendly reading without compromising quality."</p>
+          </div>
+        </section>
       </div>
       <Footer /></div>
   )
